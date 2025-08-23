b3a25d00f42d5b96ee8c0aaafd5d153896d31016
033815 <huoming.gao@fuyaogroup.com>
2025-08-23 15:14:51 +0800

    添加ipv6支持

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3003,
-    host: '0.0.0.0'
+    host: '::'
   },
   preview: {
+    port: 4173, // 这里设置你想要的预览端口号
+    host: '::', // 如果需要IPv6支持
     allowedHosts: [
       'www-mc.h385a89d9.nyat.app'
     ]
